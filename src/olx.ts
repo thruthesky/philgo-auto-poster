fafb42a25132660cbb257dbec04caa68ff266c01
gemlorenz10 <gemlorenz10@gmail.com>
2017-10-27 16:30:39 +0800

    login function

--- src/olx.ts
+++ src/olx.ts
@@ -8,11 +8,11 @@ class Olx extends PuppeteerAutoPostExtension {
 
     private item = {
         category : 'category-mobile-phones-tablets.category-mobile-phones-smartphones',
-        title : 'SamsungS7'
+        title : 'SamsungS7',
         condition : '2nd hand',
         price : '5000',
         description : 'Hello this is description, buy my phone it is good. around pampanga area only.',
-        imgPath : `C:\\image\\samsung.jpg`
+        imgPath : `C:\\image\\samsung.jpg`,
         location : {
             region : 'metro manila',
             city : 'manila'
@@ -23,7 +23,6 @@ class Olx extends PuppeteerAutoPostExtension {
         timeout : 5000,
     };
 
-
     async main() {
         await this.init( false );
         await this.firefox();
@@ -33,7 +32,8 @@ class Olx extends PuppeteerAutoPostExtension {
         this.acceptLeaveAlert();
 
         while (login) {
-            await this.philgo_get_post(this.siteName)
+            // await this.philgo_get_post(this.siteName)
+            await this.olx_test_data()
                 .then( async post => await this.open_form() )
                 .then( async () => await this.submit_form() )
                 .then( async () => await this.philgo_auto_post_log(this.post, 'SUCCESS', this.siteName, this.url + '/' + this.id))
@@ -48,14 +48,28 @@ class Olx extends PuppeteerAutoPostExtension {
     }
 
     private async login() {
-        return true
+        await this.page.goto(this.url + '/login').then( a => console.log('Loading login page') );
+        await this.page.waitForSelector('#login_button', this.waitForSelectorOptions ).then( a => console.log('Login button found') );
+        await this.page.type('input[name="mobile"]', this.id).then( a => console.log('Typing user id..') ); 
+        await this.page.type('input[name="password"]', this.password).then( a => console.log('Typing password..') );
+        await this.page.tap('#login_button').then( a => console.log('Tap login..') );
+        await this.page.waitForSelector('a[href="/ad/post"]', this.waitForSelectorOptions).then( a => console.log('Link to /ad/post found.') );
+        return true;
     }
 
     private async open_form() {
-
+        await this.page.goto(this.url + '/ad/post').then( a => console.log('Loading sell form.') );
+        await this.page.waitForSelector('.sell-button', this.waitForSelectorOptions).then( a => console.log('Sell button found.') );
     }
 
     private async submit_form() {
+        console.log('submit form');
+    }
 
+    private async olx_test_data() {
+        await this.page.waitFor(1000);
+        return this.item;
     }
 }
+
+(new Olx()).main();
