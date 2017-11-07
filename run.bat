91e90692b7f9c6408f4e3e188cd865cdfcb02513
gemlorenz10 <gemlorenz10@gmail.com>
2017-11-07 15:00:10 +0800

    start script with title

--- run.bat
+++ run.bat
@@ -1,8 +1,8 @@
-@start cmd /k title NAVER CAFE | node dist\naver-cafe.js
-@start cmd /k title KAKAO PLUS HOME | node dist\kakao-plus-home.js
-@start cmd /k title BLOGGER | node dist\blogger.js
-@start cmd /k title TWITTER | node dist\twitter.js
-@start cmd /k title TISTORY | node dist\tistory.js
-@start cmd /k title TEACHER JOB AD FACEBOOK | node dist\job-ad-facebook.js
-@start cmd /k title DAUM | node dist\daum.js
-@start cmd /k title GOOGLE PLUS | node dist\google-plus.js
+@start cmd /k "title NAVER CAFE && node dist\naver-cafe.js"
+@start cmd /k "title KAKAO PLUS HOME && node dist\kakao-plus-home.js"
+@start cmd /k "title BLOGGER && node dist\blogger.js"
+@start cmd /k "title TWITTER && node dist\twitter.js"
+@start cmd /k "title TISTORY && node dist\tistory.js"
+@start cmd /k "title TEACHER JOB AD FACEBOOK && node dist\job-ad-facebook.js"
+@start cmd /k "title DAUM && node dist\daum.js"
+@start cmd /k "title GOOGLE PLUS && node dist\google-plus.js"
